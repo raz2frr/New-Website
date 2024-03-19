01f444299fa513c6259bed2984669ec6060201bf
raz2frr <geo2greedy@gmail.com>
2024-03-19 16:39:26 -0400

    pushing my WEBSITE project

---
+++ script.js
@@ -0,0 +1,38 @@
+alert("Hello User, are you ready to start your journey?")
+let age = 10;
+if (age<10){
+    console.log("You are too young!")
+} else {
+    console.log("Welcome user! are you ready to finish tasks?!")
+}
+
+let username = 15 
+if (username>15 && username<20){
+    console.log("Username too long, please try again!")
+} else{
+    control.log("Username complishion! Continue?")
+}
+
+let isMinor = age<10;
+if (!isMinor) {
+    console.log("You are all set and met the age requirment!!")
+}
+
+let password = right;
+if (password=wrong) {
+    console.log("Wrong username! Please try again!")
+} else {
+    console.log("One moment, securing your data...")
+}
+
+// incase a minor use operation below to figure out when they can use app
+let currentAge = 7;
+let yearsToAdd = 3;
+let futureAge = currentAge + yearsToAdd;
+
+console.log("Your age after " + yearsToAdd + " years will be " + futureAge + ".");
+
+
+let myHeading = document.getElementById("myHeading")
+myWeb.innerHTML = "Welcome to To Do Website! This app is made for those who lack motivation to do simple everyday tasks. This app will help those users complete tasks in a intertaining way by earing rewards after quest complishion! ";
+
